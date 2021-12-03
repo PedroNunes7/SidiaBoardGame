e532154425136f0ba0eed52433ae9ffe392e5fc7
PedroNunes7 <89486643+PedroNunes7@users.noreply.github.com>
2021-12-02 21:06:55 -0300

    Tela de escolha de personagens feita.
    Escolha e atribuição dos status dos personagens completa.

---
+++ SidiaBoardgame/Assets/Scripts/Character/CharacterCard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterCard : MonoBehaviour
+{
+    public PlayerInfos player;
+    public CharacterStatus character;
+
+    public TextMeshProUGUI charLife;
+    public TextMeshProUGUI charDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charLife.text = "Vida : " + character.life;
+        charDamage.text = "Ataque : " + character.damage;
+    }
+
+    public void ChosenCharacter()
+    {
+        player.life = character.life;
+        player.maxLife = character.life;
+        player.damage = character.damage;
+        player.maxDamage = character.damage;
+    }
+}
---
+++ SidiaBoardgame/Assets/Scripts/Character/CharacterStatus.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterStatus", menuName = "VariableReferences/CharacterStatus")]
+public class CharacterStatus : ScriptableObject
+{
+    public int life;
+    public int damage;
+}
